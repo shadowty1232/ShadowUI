4be0ea628d6f7692566f40df7e5c5bbc3ef6d8bb
BuildTools <unconfigured@null.spigotmc.org>
2021-07-03 18:48:49 +0100

    Initial commit

---
+++ src/main/java/uk/co/tmdavies/shadowui/ShadowUI.java
@@ -0,0 +1,17 @@
+package uk.co.tmdavies.shadowui;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class ShadowUI extends JavaPlugin {
+
+	@Override
+	public void onEnable() {
+		// Plugin startup logic
+
+	}
+
+	@Override
+	public void onDisable() {
+		// Plugin shutdown logic
+	}
+}
